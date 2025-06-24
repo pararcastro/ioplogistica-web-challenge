c8c0d2c1af4c0d49e0bfd07c2e8321c36121b51c
rodrigo_c <rcastro@paradigmadigital.com>
2025-06-24 19:16:47 +0200

    test: useDebounce hook test minimal testing requirments with delays applied

---
+++ zara-challenge/src/hooks/useDebounce.test.ts
@@ -0,0 +1,33 @@
+import { describe, test, expect, vi, beforeEach, afterEach } from 'vitest'
+import { renderHook } from '@testing-library/react'
+import { useDebounce } from './useDebounce'
+import { act } from 'react'
+
+describe('useDebounce', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  test('delays value update until delay period passes', () => {
+    const { result, rerender } = renderHook(
+      ({ value, delay }) => useDebounce(value, delay),
+      { initialProps: { value: 'initial', delay: 500 } }
+    )
+
+    expect(result.current).toBe('initial')
+
+    rerender({ value: 'updated', delay: 500 })
+     
+    expect(result.current).toBe('initial')
+    
+    act(()=> {
+         vi.advanceTimersByTime(500)
+    })
+    
+    expect(result.current).toBe('updated')
+  })
+})
